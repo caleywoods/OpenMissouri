1510f0be52c917e4ac01a1b22398f8ec3d8f9cb4
Caley Woods <caley.w@gmail.com>
2011-07-07 22:47:22 -0500

    added jQuery-tokeninput plugin for data_sets#new

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,26 @@
 class CategoriesController < ApplicationController
   inherit_resources
-  
+
+  def index
+    # change this to something like category.where("name like %?$", params[:q]) if
+    # you want to filter on just categories matching what was typed.
+    @categories = Category.all
+
+    respond_to do |format|
+      # .map(&:attributes) is removing the json root object.
+      # this will perform better in 1.9.2 as it handles the Symbol#to_proc better.
+      format.json { render :json => @categories.map(&:attributes), :only => %w[id name] }
+    end
+  end
+
   def show
-    @categories = Category.published_categories    
+    @categories = Category.published
     @category = Category.find(params[:id])
     @data_sets = @category.data_sets.paginate :page => params[:page], :conditions => {:status => "published"}, :order => 'data_sets.created_at desc', :per_page => 20
-    
+
     show!
   end
-  
+
   protected
 
   def collection
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,14 @@
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :data_sets
 
-  def self.published_categories  
-    find(:all, :conditions => "id in (#{DataSet.find(:all, :conditions => {:status => "published"}).collect{|data_set| data_set.categories.collect{|category| category.id}}.flatten.join(",")})", :order => "name")
-  end 
+  attr_reader :category_tokens
+  attr_accessible :category_tokens
+
+  def category_tokens=(ids)
+    self.category_ids = ids.split(",")
+  end
+
+  scope :published, lambda {
+    joins(:data_sets).
+    where("data_sets.status = 'published'") }
 end
